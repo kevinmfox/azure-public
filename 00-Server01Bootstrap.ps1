b0e9af2ab09bf5d79ecf0e136cc3825f9003ace5
Kevin F <kevinmfox@hotmail.com>
2021-01-24 12:20:56 -0500

    added bootstrap for server01

---
+++ 00-Server01Bootstrap.ps1
@@ -0,0 +1,7 @@
+﻿New-Item -Path "C:\Bootstrap" -ItemType Directory
+
+(New-Object System.Net.WebClient).DownloadFile(`
+    "https://raw.githubusercontent.com/kevinmfox/azure-public/master/00-Server01Bootstrap.ps1",`
+    "C:\Bootstrap\01-Server01Bootstrap.ps1")
+
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
