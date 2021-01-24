098077693f035ed7583953f0e279fc1b649cbcda
Kevin F <kevinmfox@hotmail.com>
2021-01-24 11:48:59 -0500

    script updates

--- 00-DC01Bootstrap.ps1
+++ 00-DC01Bootstrap.ps1
@@ -1,29 +1,33 @@
-﻿$ipAddress = "192.168.40.20"
-$defaultGateway = "192.168.40.1"
+﻿function GeneratePassword([Int]$Length)
+{
+    $chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPRSTUVWXYZ23456789"
+    $password = ""
+    for ($i = 0; $i -lt $Length; $i++)
+    {
+        $password += $chars.ToCharArray() | Get-Random
+    }
+    return $password
+}
 
 New-Item -Path "C:\Bootstrap" -ItemType Directory
 
 (New-Object System.Net.WebClient).DownloadFile(`
     "https://raw.githubusercontent.com/kevinmfox/azure-public/master/00-DC01Bootstrap.ps1",`
-    "C:\Bootstrap\00-DC01Bootstrap.ps1")
-
-(New-Object System.Net.WebClient).DownloadFile(`
-    "https://raw.githubusercontent.com/kevinmfox/azure-public/master/01-DC01Bootstrap.ps1",`
     "C:\Bootstrap\01-DC01Bootstrap.ps1")
-
-New-NetIPAddress -IPAddress $ipAddress -InterfaceAlias Ethernet0 -DefaultGateway $defaultGateway -AddressFamily IPv4 -PrefixLength 24
-Set-DnsClientServerAddress -InterfaceAlias Ethernet0 -ServerAddresses 127.0.0.1, 8.8.8.8
 
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
-Register-ScheduledTask `
-    -TaskName 01DC01Bootstrap `
-    -Action (New-ScheduledTaskAction `
-        -Execute powershell.exe `
-        -Argument "C:\Bootstrap\01-DC01Bootstrap.ps1") `
-    -Trigger (New-ScheduledTaskTrigger -AtStartup) `
-    -Principal (New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest)
+$safeModePassword = GeneratePassword -Length 32
 
-Start-Sleep -Seconds 120
+$safeModePassword | Out-File -FilePath "C:\Bootstrap\SafeMode.txt" -Append:$false -Force
 
-Rename-Computer -NewName DC01 -Restart
+Install-ADDSForest `
+    -SkipPreChecks `
+    -DomainName fox.local `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText $safeModePassword -Force) `
+    -CreateDnsDelegation:$false `
+    -DomainMode WinThreshold `
+    -DomainNetbiosName FOX `
+    -ForestMode WinThreshold `
+    -InstallDns `
+    -Force
---
+++ Archived/00-DC01Bootstrap.ps1
@@ -0,0 +1,30 @@
+﻿$ipAddress = "10.10.20.20"
+$defaultGateway = "10.10.20.1"
+$adapterName = "Ethernet"
+
+New-Item -Path "C:\Bootstrap" -ItemType Directory
+
+(New-Object System.Net.WebClient).DownloadFile(`
+    "https://raw.githubusercontent.com/kevinmfox/azure-public/master/00-DC01Bootstrap.ps1",`
+    "C:\Bootstrap\00-DC01Bootstrap.ps1")
+
+(New-Object System.Net.WebClient).DownloadFile(`
+    "https://raw.githubusercontent.com/kevinmfox/azure-public/master/01-DC01Bootstrap.ps1",`
+    "C:\Bootstrap\01-DC01Bootstrap.ps1")
+
+New-NetIPAddress -IPAddress $ipAddress -InterfaceAlias $adapterName -DefaultGateway $defaultGateway -AddressFamily IPv4 -PrefixLength 24
+Set-DnsClientServerAddress -InterfaceAlias $adapterName -ServerAddresses 127.0.0.1, 8.8.8.8
+
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+Register-ScheduledTask `
+    -TaskName 01DC01Bootstrap `
+    -Action (New-ScheduledTaskAction `
+        -Execute powershell.exe `
+        -Argument "C:\Bootstrap\01-DC01Bootstrap.ps1") `
+    -Trigger (New-ScheduledTaskTrigger -AtStartup) `
+    -Principal (New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest)
+
+Start-Sleep -Seconds 120
+
+Rename-Computer -NewName DC01 -Restart
